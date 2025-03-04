9906d75c56c208e85c9a9226d7c3eb17689b4a31
Vanessa Valencia-Garcia <vanessa_vg@outlook.fr>
2025-03-04 13:12:28 +0100

    Etape 3

--- script.js
+++ script.js
@@ -40,3 +40,31 @@ function translateLatinCharacter(character) {
     console.log(translateLatinCharacter('A'));
     console.log((translateLatinCharacter('T')));
     console.log((translateLatinCharacter('X')));
+
+
+function encode (text) {
+     return getLatinCharacterList(text)
+     .map(character => translateLatinCharacter(character.toUpperCase()))
+    .join(' ');
+}   
+
+console.log(encode('HELLOOO'));
+console.log(encode('VACANCES'));
+console.log(encode(`Le code c'est génial !`));
+
+// Version plus détaillée de function encode:
+
+// function encode(text) {
+//     const morseCodeArray = [];
+
+// Découper le texte, et pour chaque caractère, on le transforme en majuscule et en morse
+//     for (let i = 0; i < text.length; i++) {  // Parcourir chaque caractère du tableau
+//         const character2 = text[i].toUpperCase();  // Convertir en majuscule
+//         const morse = translateLatinCharacter(character2) || '?';  // Trouver le morse ou '?' si non trouvé
+//         morseCodeArray.push(morse);  // Ajouter le resultat dans le tableau
+//     }
+
+//     return morseCodeArray.join(' ');  // Retourner les codes morses séparés par des espaces
+// }
+
+// console.log(encode('HELLO'));
